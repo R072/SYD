160a69de29d91b1f096b861892adb670f4a2446d
R072 <wjr3765@hotmail.com>
2016-05-25 01:51:06 +0800

    haha

--- main.c
+++ main.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
+#define ZERO 0 
+
 int remembercount(int);
 
 int main(int argc, char *argv[])
 {
   int R1;
+
+  R1 = ZERO;
+  
   printf("Start, untill the end.\n");
   R1 = remembercount(0);
 }
@@ -13,3 +18,5 @@ int remembercount(int a)
   if(a == 0) printf("\n The number is \"%d\" of times \n", a);
   return 0;
 }
+
+
